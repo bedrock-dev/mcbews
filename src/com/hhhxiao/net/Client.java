451f538fe70dbd7ab31b1c8fe3254692c6519643
hhhxiao <1417599491@qq.com>
2019-12-04 12:06:11 +0800

    添加sendCommandSet方法，以修复指令发送过快导致游戏执行不过来的问题。目前16w条指令连续（雾）执行无压力。

--- src/com/hhhxiao/net/Client.java
+++ src/com/hhhxiao/net/Client.java
@@ -5,24 +5,46 @@ import com.hhhxiao.Request.RequestBuilder;
 import com.hhhxiao.Request.RequestPurpose;
 import org.java_websocket.WebSocket;
 
+import java.util.List;
+import java.util.Queue;
+
 public class Client {
     private final WebSocket webSocket;
+
+    private Queue<Runnable> tasks;
 
     public Client(WebSocket w) {
         webSocket = w;
     }
 
-    public void  send(Request request){
+    public void send(Request request) {
         webSocket.send(request.toJSONString());
     }
 
-    public void sendCommand(String command){
+    public void sendCommand(String command) {
         Request request = new RequestBuilder(RequestPurpose.COMMAND_REQUEST)
                 .commandBody(command).build();
         send(request);
     }
 
-    public void subscribe(String event){
+    public void sendCommandSet(List<String> commands) {
+
+        int index = 0;
+        for (String command : commands) {
+            if (index % 100 == 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            index++;
+            sendCommand(command);
+        }
+    }
+
+
+    public void subscribe(String event) {
         Request request = new RequestBuilder(RequestPurpose.SUBSCRIBE)
                 .eventBody(event).build();
         send(request);
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,57 @@
 package test;
 
-import com.hhhxiao.debug.Task;
+import com.hhhxiao.Request.EventName;
+import com.hhhxiao.debug.Logger;
+import com.hhhxiao.net.Client;
+import com.hhhxiao.net.EmptyListener;
+import com.hhhxiao.net.Listener;
+import com.hhhxiao.net.MCWebSocket;
+import com.hhhxiao.resp.CommandResponse;
+import com.hhhxiao.resp.EventResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class MyListener extends EmptyListener {
+    private boolean flag = true;
+
+    @Override
+    public void onConnected(Client client) {
+        super.onConnected(client);
+        client.subscribe(EventName.PLAYER_MESSAGE);
+    }
+
+    @Override
+    public void onEventResponse(Client client, EventResponse response) {
+        super.onEventResponse(client, response);
+        if (response.getEventName().equals(EventName.PLAYER_MESSAGE)) {
+            if (response.getMessage().startsWith("test")) {
+                String[] str = response.getMessage().split(" ");
+                final int x = Integer.parseInt(str[1])/2;
+                final int y = Integer.parseInt(str[2])/2;
+                List<String> commands = new ArrayList<>();
+                for (int i = -x; i < x; i++) {
+                    for (int j = -y; j < y; j++) {
+                        commands.add("/setblock ~" + i + "~4~" + j + " stone");
+                    }
+                }
+                commands.add("/say placed finished");
+                new Thread(() -> client.sendCommandSet(commands)).start();
+            }
+        }
+    }
+
+    @Override
+    public void onCommandResponse(Client client, CommandResponse response) {
+        super.onCommandResponse(client, response);
+        Logger.d(response.getStatusCode()+" : "+response.getStatusMessage());
+    }
+}
 
 public class Test {
     public static void main(String[] args) {
-        Task task = new Task(()->System.out.println("1212"));
-        task.start();
+        MCWebSocket webSocket = new MCWebSocket("127.0.0.1", 1234);
+        webSocket.addListener(new MyListener());
+        webSocket.run();
     }
 }
